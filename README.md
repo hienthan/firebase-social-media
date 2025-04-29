a3b195d326263050d45ebab16ede41dd59f22e58
hienthan <hien.thanquang@gmail.com>
2025-04-29 17:06:27 +0700

    Removed sensitive stuff.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 `https://www.youtube.com/watch?v=f55qeKGgB_M&list=PLpPqplz6dKxW5ZfERUPoYTtNUNvrEebAR&index=19`
 
-Setup firebase then
+- Setup firebase then
 > npm install firebase
-The hooks useAuthState:
+- The hooks useAuthState:
 >npm install react-firebase-hooks
-Implemented the login/logout logic.
+- Implemented the login/logout logic.
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,12 +6,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB5KJ0jAAC21qwZfzCtDtI0Cz8dSuA_d3c",
-  authDomain: "social-media-82989.firebaseapp.com",
-  projectId: "social-media-82989",
-  storageBucket: "social-media-82989.firebasestorage.app",
-  messagingSenderId: "635414576800",
-  appId: "1:635414576800:web:4c4e53b056551b909093c5",
+
 };
 
 // Initialize Firebase
